539f6e599cd96d363f2e4f76f0f35c279a817907
Maksim Makhanko <tolkoo.keeper@gmail.com>
2022-08-19 20:50:13 +0300

    Initial

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -1,2 +1,14 @@
-package com.polarbookshop.edgeservice.config;public class RateLimiterConfig {
+package com.polarbookshop.edgeservice.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just("anonymous");
+    }
 }
